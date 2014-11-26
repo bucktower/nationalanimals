5e935feb17a6e887955396a9ef91eac2b2aa4356
Buck <bucktower14@gmail.com>
2014-11-26 01:59:02 -0600

    initial commit

---
+++ src/net/bucktower/nationalanimals/Nationalanimals.java
@@ -0,0 +1,17 @@
+package net.bucktower.nationalanimals;
+
+import processing.core.PApplet;
+
+
+public class Nationalanimals extends PApplet {
+
+	public void setup() {
+	}
+
+	public void draw() {
+	}
+	
+	public static void main(String _args[]) {
+		PApplet.main(new String[] { net.bucktower.nationalanimals.Nationalanimals.class.getName() });
+	}
+}
